6eeb35f1dcada92329019b5a4b0991f5e25e2ef1
locnv14 <locnv14@gmail.com>
2025-01-21 00:09:07 +0700

    update

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,5 +1,5 @@
 /**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
+ * Updated by locnv14's author on JAN 20 2025
+ * Email: locnv14@gmail.com
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
